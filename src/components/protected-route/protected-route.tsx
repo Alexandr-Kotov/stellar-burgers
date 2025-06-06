c16e277f19064c3a3687ca41a2b78a10f4fd4fdb
Kotov Alexandr <Mr1Hiroshi@yandex.ru>
2025-06-06 09:23:16 +0500

    Route

---
+++ src/components/protected-route/protected-route.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable arrow-body-style */
+type ProtectedRouteProps = {
+  children: React.ReactElement;
+};
+
+export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  return children;
+};
